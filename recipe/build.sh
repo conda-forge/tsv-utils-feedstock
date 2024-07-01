c026b439786303ac8d816c6c007f878906ad9977
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-01 22:54:32 +0200

    Try without dflags, they might not be needed

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,7 @@
 
 set -x
 
-DFLAGS="-L-L${PREFIX}/lib -L-Wl,-rpath,${PREFIX}/lib"
-if [[ "$target_platform" == "osx-64" ]]; then
-    export DFLAGS=""
-fi
-
-make DCOMPILER=ldc2 DFLAGS="$DFLAGS"
+make DCOMPILER=ldc2
 make test-nobuild DCOMPILER=ldc2
 
 cp -r bin $PREFIX
