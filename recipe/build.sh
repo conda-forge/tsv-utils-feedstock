e816ebc5ede977de00ed7f6cffdee0bb4f623d21
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-01 20:58:58 +0200

    try more

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,12 @@ if [[ "$target_platform" == "osx-64" ]]; then
     export DFLAGS=""
 fi
 
+ls ${PREFIX}/lib | grep ldc
+
+# Ensure the ldc2 and necessary libraries are correctly linked
+export LIBRARY_PATH="${PREFIX}/lib:${LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
+
 make DCOMPILER=ldc2 DFLAGS="$DFLAGS"
 make test-nobuild DCOMPILER=ldc2
 
