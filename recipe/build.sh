dea456d3227877ee08b3bceb058a5d2e77a7e1be
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-02 00:15:24 +0200

    Use downloaded compiler directly

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+LDC_VERSION=1.38.0
+
 set -x
+
+if [ "$target_platform" == "osx-64" ]; then
+    ARCHSTR="osx-x86_64"
+else
+    ARCHSTR="linux-x86_64"
+fi
+
+wget https://github.com/ldc-developers/ldc/releases/download/v${LDC_VERSION}/ldc2-${LDC_VERSION}-${ARCHSTR}.tar.xz
+tar -xf ldc2-${LDC_VERSION}-${ARCHSTR}.tar.xz
+PATH=$PWD/ldc2-${LDC_VERSION}-${ARCHSTR}/bin:$PATH
 
 make DCOMPILER=ldc2 DFLAGS="-link-defaultlib-shared=false"
 
