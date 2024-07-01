688219a176d97edc8c3d7f9d3af40d7b3bfd4be7
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-02 00:54:01 +0200

    Statically link the d libraries

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,15 +4,8 @@ LDC_VERSION=1.26.0
 
 set -x
 
-if [ "$target_platform" == "osx-64" ]; then
-    ARCHSTR="osx-x86_64"
-else
-    ARCHSTR="linux-x86_64"
-fi
-
-wget https://github.com/ldc-developers/ldc/releases/download/v${LDC_VERSION}/ldc2-${LDC_VERSION}-${ARCHSTR}.tar.xz
-tar -xf ldc2-${LDC_VERSION}-${ARCHSTR}.tar.xz
-PATH=$PWD/ldc2-${LDC_VERSION}-${ARCHSTR}/bin:$PATH
+curl -fsS https://dlang.org/install.sh | bash -s install ldc-$LDC_VERSION
+source ~/dlang/ldc-$LDC_VERSION/activate
 
 make DCOMPILER=ldc2 DFLAGS="-link-defaultlib-shared=false"
 
