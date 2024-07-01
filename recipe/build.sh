61275e03a9a192885b2648c92aedf464ff6a2d4b
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-01 20:42:45 +0200

    Try removing requirement for ldc at run time for osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,12 @@
 
 set -x
 
-make DCOMPILER=ldc2 DFLAGS="-L-L${PREFIX}/lib -L-Wl,-rpath,${PREFIX}/lib"
+DFLAGS="-L-L${PREFIX}/lib -static"
+if [[ "$target_platform" == "osx-64" ]]; then
+    export DFLAGS=""
+fi
+
+make DCOMPILER=ldc2 DFLAGS="$DFLAGS"
 make test-nobuild DCOMPILER=ldc2
 
 cp -r bin $PREFIX
