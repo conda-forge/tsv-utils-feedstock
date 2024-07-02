b95ae4cd3a861ff8dc044ccbda18a86373c96178
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-02 16:23:09 +0200

    Add osx-arm64 compiler

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-LDC_VERSION=1.26.0
+set -euxo pipefail
 
-set -x
-
-curl -fsS https://dlang.org/install.sh | bash -s install ldc-$LDC_VERSION
-source ~/dlang/ldc-$LDC_VERSION/activate
+if [[ "$target_platform" == osx-arm64 ]]; then
+    LDC_VERSION=1.38.0
+    curl -fsS https://dlang.org/install.sh | bash -s install ldc-$LDC_VERSION
+    source ~/dlang/ldc-$LDC_VERSION/activate
+fi
 
 make DCOMPILER=ldc2 DFLAGS="-link-defaultlib-shared=false"
 
