30531ff1a13feb32b1f36fb1a857eeb2de46dc77
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-03 01:43:29 +0200

    Build number 6, now with osx-arm64 support! yay!

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,7 @@ MTRIPLE=""
 
 if [[ "$target_platform" == "osx-arm64" ]]; then
     # Get the arm64 lib files
+    # Ideally, we would get these from the conda-forge ldc2 arm64 package, but it is not available yet
     LDC_VERSION=1.38.0
     LDC_ARCH=osx-arm64
     wget https://github.com/ldc-developers/ldc/releases/download/v$LDC_VERSION/ldc2-$LDC_VERSION-$LDC_ARCH.tar.xz
@@ -24,7 +25,6 @@ if [[ "$target_platform" == "osx-arm64" ]]; then
                 "-Xcc=arm64",
                 "-Xcc=-isysroot",
                 "-Xcc=$CONDA_BUILD_SYSROOT",
-                "-L-v",
             ];
             lib-dirs = [
                 "$BUILD_PREFIX/lib-arm64",
