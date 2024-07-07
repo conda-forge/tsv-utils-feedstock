e62e4d4778079f528e52b3a39f64b03fee673374
Cornelius Roemer <cornelius.roemer@gmail.com>
2024-07-07 12:57:07 +0200

    f

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,8 @@ MTRIPLE=""
 if [[ "$target_platform" == "osx-arm64" ]]; then
     # Get the arm64 lib files
     # Ideally, we would get these from the conda-forge ldc2 arm64 package, but it is not available yet
-    LDC_VERSION=1.39.0
+    latest_version=$(curl -s https://api.github.com/repos/ldc-developer/ldc/releases/latest | jq -r .tag_name | sed 's/^v//')
+    LDC_VERSION=latest_version
     LDC_ARCH=osx-arm64
     wget https://github.com/ldc-developers/ldc/releases/download/v$LDC_VERSION/ldc2-$LDC_VERSION-$LDC_ARCH.tar.xz
     tar -xf ldc2-$LDC_VERSION-$LDC_ARCH.tar.xz
